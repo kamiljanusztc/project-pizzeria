3b399831a42bc50143d3c0f3306180a03e6ffc48
Kamil Janusz <kamiljanusztc@gmail.com>
2020-12-01 22:25:51 +0100

    Add selector to script.js

--- src/js/script.js
+++ src/js/script.js
@@ -86,24 +86,28 @@
       const thisProduct = this;
 
       /* find the clickable trigger (the element that should react to clicking) */
-      const clickableTrigger = ???;
+      const clickableTrigger = document.querySelector(select.menuProduct.clickable);
 
       /* START: add event listener to clickable trigger on event click */
       clickableTrigger.addEventListener('click', function(event) {
 
         /* prevent default action for event */
-
+        event.preventDefault();
 
         /* find active product (product that has active class) */
+        const activeProduct = document.querySelector(select.all.menuProductsActive);
 
         /* if there is active product and it's not thisProduct.element, remove class active from it */
+        if(activeProduct != thisProduct.element) {
+        activeProduct.classList.remove('active');
+        }
 
         /* toggle active class on thisProduct.element */
+        thisProduct.element.classList.toggle('active');
 
       });
     }
   }
-
 
   const app = {
     initData: function() {
