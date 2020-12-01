84266b0b65b0d000cb1d0d4e2c7c79cc38f0da7c
Kamil Janusz <kamiljanusztc@gmail.com>
2020-12-01 19:43:06 +0100

    Add comment to script.js

--- src/js/script.js
+++ src/js/script.js
@@ -56,14 +56,36 @@
     constructor(id, data) {
       const thisProduct = this;
 
+      thisProduct.id = id;
+      thisProduct.data = data;
+
+      thisProduct.renderInMenu(); //the constructor will run this function after creating the instance
+
       console.log('new Product:', thisProduct);
+    } // klasa product za pomoca metody renderInMenu bierze dane zrodlowe produktu i wyrzuca je do szablonu - tak powstaje kod html pojedynczego produktu
+
+    renderInMenu() {
+      const thisProduct = this;
+
+      /* generate HTML based on template */
+      const generatedHTML = templates.menuProduct(thisProduct.data);
+
+      /* create element using utils.createElementFromHTML */
+      thisProduct.element = utils.createDOMFromHTML(generatedHTML);
+
+      /* find menu container */
+      const menuContainer = document.querySelector(select.containerOf.menu);
+
+      /* add element to menu */
+      menuContainer.appendChild(thisProduct.element);
     }
   }
+
 
   const app = {
     initData: function() {
       const thisApp = this;
-      thisApp.data = dataSource;  //reference (address) to data
+      thisApp.data = dataSource; //reference (address) to data
     },
 
     initMenu: function() {
