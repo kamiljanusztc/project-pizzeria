47a9fc5d792e1209013ba3bae6cc793f6025e2e8
Kamil Janusz <kamiljanusztc@gmail.com>
2020-12-03 19:23:29 +0100

    Add variable to processOrder in script.js

--- src/js/script.js
+++ src/js/script.js
@@ -130,11 +130,6 @@
     initOrderForm() { // metoda odpowiedzialna za dodanie listenerow eventow do formularza, jego kontrolek i guzika dodania do koszyka
       const thisProduct = this;
       console.log(this.initOrderForm);
-    }
-
-    processOrder() {
-      const thisProduct = this;
-      console.log(this.processOrder);
 
       thisProduct.form.addEventListener('submit', function(event){
         event.preventDefault();
@@ -154,6 +149,37 @@
 
       const formData = utils.serializeFormToObject(thisProduct.form);
       console.log('formData', formData);
+    }
+
+    processOrder() {
+      const thisProduct = this;
+      console.log(this.processOrder);
+
+      // covert form to object structure e.g. { sauce: ['tomato'], toppings: ['olives', 'redPeppers']}
+      const formData = utils.serializeFormToObject(thisProduct.form);
+      console.log('formData', formData);
+
+      // set price to default price
+      let price = thisProduct.data.price;
+
+      // for every category (param)...
+      for(let paramId in thisProduct.data.params) {
+
+        // determine param value, e.g. paramId = 'toppings', param = { label: 'Toppings', type: 'checkboxes'... }
+        const param = thisProduct.data.params[paramId];
+        console.log(paramId, param);
+
+        // for every option in this category
+        for(let optionId in param.options) {
+
+          // determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
+          const option = param.options[optionId];
+          console.log(optionId, option);
+        }
+      }
+
+      // update calculated price in the HTML
+      thisProduct.priceElem.innerHTML = price;
     }
   }
 
